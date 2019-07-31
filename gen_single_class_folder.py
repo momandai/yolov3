4213b45be8267c014d5a1c457630cadc35129109
neptune <379853283@qq.com>
2019-07-31 18:52:57 +0800

    gen single class

---
+++ gen_single_class_folder.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import shutil
+
+in_file = "../coco/trainvalno5k.txt"
+
+output_label_dir = "../coco/person_labels"
+output_image_dir = "../coco/person_images"
+output_abstract_file = "./data/coco_person.txt"
+output_abstract_file_np = None
+if os.path.exists(output_label_dir):
+    shutil.rmtree(output_label_dir)
+os.makedirs(output_label_dir)
+
+if os.path.exists(output_image_dir):
+    shutil.rmtree(output_image_dir)
+os.makedirs(output_image_dir)
+
+out_file = "data/coco_person.data"
+img_formats = ['.bmp', '.jpg', '.jpeg', '.png', '.tif']
+with open(in_file, 'r') as f:
+    img_files = f.read().splitlines()
+    img_files = [x for x in img_files if os.path.splitext(x)[-1].lower() in img_formats]
+
+    # label_files = [x.replace('images', 'labels').replace(os.path.splitext(x)[-1], '.txt')
+    #                     for x in img_files]
+
+    for img_file in img_files[:100]:
+        label_file = img_file.replace('images', 'labels').replace(os.path.splitext(img_file)[-1], '.txt')
+        if os.path.isfile(label_file):
+            with open(label_file, 'r') as f_label_file:
+                x = np.array([x.split() for x in f_label_file.read().splitlines()], dtype=np.float32)
+                y = None
+                has_person = False
+                for i in range(x.shape[0]):
+                    if x[i, 0] == 0:
+                        has_person = True
+                        y = np.vstack((y, [x[i, :]])) if y is not None else np.array([x[i, :]])
+                if has_person:
+                    output_label_file = os.path.join(output_label_dir, os.path.basename(label_file))
+                    output_image_file = os.path.join(output_image_dir, os.path.basename(img_file))
+                    shutil.copy(img_file, output_image_file)
+                    np.savetxt(output_label_file, y, fmt='%.6f')
+                    output_abstract_file_np = np.vstack((output_abstract_file_np, [output_image_file])) \
+                        if output_abstract_file_np is not None else np.array([output_image_file])
+
+    np.savetxt(output_abstract_file, output_abstract_file_np, fmt='%s')
